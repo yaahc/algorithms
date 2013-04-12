272a84ea687780a65dccadf54cad7b1b2fbf4b4c
John Lusby <jlusby42@gmail.com>
2013-04-12 22:54:18 +0000

    finished lab 8

--- Lab08/matrixPrinting.cpp
+++ Lab08/matrixPrinting.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 #include <limits.h>
 using namespace std;
 
-void printOptimalParens(int** s, int i, int j) {
+void printOptimalParens(vector< vector<int> > s, int i, int j) {
     if(i == j)
-        cout << "A" << i;
+        cout << "A" << i-1;
     else {
         cout << "(";
         printOptimalParens(s, i, s[i][j]);
@@ -15,17 +16,17 @@ void printOptimalParens(int** s, int i, int j) {
 }
 
 void matrixChainOrder(int* p, int n) {
-    int m[n][n];
-    int **s = new int*[n];
+    vector< vector<int> > m(n, vector< int >(n));
+    vector< vector<int> > s(n, vector< int >(n));
     for(int i = 0; i < n; i++) {
-        s[i] = new int[n];
         m[i][i] = 0;
     }
-    for(int l = 2; l <= n; l++) {
-        for(int i = 1; i <= n - l + 1; i++) {
+    for(int l = 2; l < n; l++) {
+        for(int i = 1; i < n - l + 1; i++) {
             int j = i + l - 1;
             m[i][j] = INT_MAX;
             for(int k = i; k <= j - 1; k++) {
+                //cout << "i:" << i << " j:" << j << " k:" << k << endl;
                 int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
                 if(q < m[i][j]) {
                     m[i][j] = q;
@@ -48,6 +49,7 @@ int main() {
             cin >> p[i];
         matrixChainOrder(p, size+1);
         cin >> size;
+        delete [] p;
     }
 }
 
